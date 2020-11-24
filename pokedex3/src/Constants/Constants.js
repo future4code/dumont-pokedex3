13a74cedd0a3b182efa451f7c5cc3c7ce3019ed7
Josewd <63750044+Josewd@users.noreply.github.com>
2020-11-24 10:34:20 -0300

    adicionei cores lights e darks na palleta e coloquei comentario para forma de uso

--- pokedex3/src/Constants/Constants.js
+++ pokedex3/src/Constants/Constants.js
@@ -30,5 +30,6 @@ export const palletaCores = {
      red : {primary:'#F52C33', light: '#ff695e', dark: '#ba000b', text:'#000000'},
      green : {primary:'#7ABF5A', light: '#acf289', dark: '#498e2c', text:'#000000'},
      blue : {primary:'#05DBF2', light: '#6cffff', dark: '#00a9bf', text:'#000000'},
-     purple : {primary:'#451659', light: '#734186', dark: '#1f002f', text:'#ffffff'}
+     purple : {primary:'#451659', light: '#734186', dark: '#1f002f', text:'#ffffff'},
+     black :  {primary:'#232323', light: '#30333A', dark: '#020202', text:'#ffffff'}
 }
